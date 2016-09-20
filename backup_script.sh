29622d39c5034a2911ba298271a4e60bdaa1e511
vangyyy <rasto.vangor@gmail.com>
2016-09-20 20:07:41 +0200

    added ".IdeaIC2016.2"

--- backup_script.sh
+++ backup_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # What to backup
-backup_files="/home/$USER/.ssh /home/$USER/.gimp-2.8 /home/$USER/.thunderbird /home/$USER/.mozilla /home/$USER/.atom /home/$USER/.bashrc /home/$USER/.gitconfig /home/$USER/.smartgit /home/$USER/.shutter /home/$USER/.gconf /home/$USER/.gnome /home/$USER/.gnome2 /home/$USER/.compiz /home/$USER/.local /home/$USER/.config /home/$USER/Pictures /home/$USER/Calibre_library /home/$USER/bin /usr/share/icons/My-Custom"
+backup_files="/home/$USER/.ssh /home/$USER/.gimp-2.8 /home/$USER/.thunderbird /home/$USER/.mozilla /home/$USER/.atom /home/$USER/.bashrc /home/$USER/.gitconfig /home/$USER/.smartgit /home/$USER/.shutter /home/$USER/.gconf /home/$USER/.gnome /home/$USER/.gnome2 /home/$USER/.compiz /home/$USER/.IdeaIC2016.2 /home/$USER/.local /home/$USER/.config /home/$USER/Pictures /home/$USER/Calibre_library /home/$USER/bin /usr/share/icons/My-Custom"
 
 # Where to backup to
 dest="/home/$USER/Backups"

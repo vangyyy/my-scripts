df2c33ce1acad965283fd101addc0f076cbf2156
vangyyy <rasto.vangor@gmail.com>
2016-09-27 18:58:54 +0200

    restored --purge switch

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ sudo apt-get -y update
 sudo apt-get -y dist-upgrade
 
 # Remove bloat
-sudo apt-get -y remove aisleriot gnome-mahjongg gnome-mines gnome-sudoku gnome-calendar checkbox-gui totem example-content transmission-common transmission-gtk brltty onboard xdiagnose webbrowser-app apport
+sudo apt-get -y remove --purge aisleriot gnome-mahjongg gnome-mines gnome-sudoku gnome-calendar checkbox-gui totem example-content transmission-common transmission-gtk brltty onboard xdiagnose webbrowser-app apport
 sudo apt-get -y autoremove && sudo apt-get -y clean all && sudo apt-get -y autoclean all
 
 # Remove useless icons

ed8f642f6519f7714a35d33d7fdaa8b887ae3519
vangyyy <rasto.vangor@gmail.com>
2017-05-22 23:04:51 +0200

    update unity setup script

--- unity_setup.sh
+++ unity_setup.sh
@@ -1,15 +1,58 @@
 #!/bin/bash
 
 # http://askubuntu.com/questions/761180/wifi-doesnt-work-after-suspend-after-16-04-upgrade
-# http://askubuntu.com/questions/142128/open-windows-in-center-of-screen
-# Android Studio
+# gksu service network-manager restart
+# gksu systemctl restart network-manager.service
+# Android Studio, packettracer, sqldatabase
+
+# Copy files
+while true; do
+	read -p "Do you want to copy icon packs ? [Y/n] " answer
+	if [[ ($answer = "y") || ($answer = "Y") ]]; then
+		if [ -d "My-Custom" ]; then
+			sudo cp -a ./My-Custom /usr/share/icons
+			echo "My-Custom copied."
+			break
+		else
+			echo "My-Custom does not exist. Aborting ..."
+			exit 1
+		fi
+
+		if [ -d "My-Custom-Dark" ]; then
+			sudo cp -a ./My-Custom-Dark /usr/share/icons
+			echo "My-Custom-Dark copied."
+			break
+		else
+			echo "My-Custom-Dark does not exist. Aborting ..."
+			exit 1
+		fi
+	elif [[ ($answer = "n") || ($answer = "N") ]]; then
+		break
+	fi
+done
+
+while true; do
+	read -p "Do you want to copy wallpapers ? [Y/n] " answer
+	if [[ ($answer = "y") || ($answer = "Y") ]]; then
+		if [ -d "Wallpapers" ]; then
+			cp -a ./Wallpapers/ ~/Pictures/
+			echo "Wallpapers copied."
+			break
+		else
+			echo "Wallpapers folder does not exist. Aborting ..."
+			exit 1
+		fi
+	elif [[ ($answer = "n") || ($answer = "N") ]]; then
+		break
+	fi
+done
 
 # Update and Upgrade
 sudo apt-get -y update
 sudo apt-get -y dist-upgrade
 
-# Virtualbox
-sudo adduser $USER vboxusers
+# Virtualbox (uncomment)
+#sudo adduser $USER vboxusers
 
 # Grub
 sudo apt-get -y remove memtest86+
@@ -24,10 +67,12 @@ sudo add-apt-repository -y ppa:mmk2410/intellij-idea-community
 sudo add-apt-repository -y ppa:damien-moore/codeblocks-stable
 sudo add-apt-repository -y ppa:docky-core/stable
 sudo add-apt-repository -y ppa:noobslab/macbuntu
-sudo add-apt-repository -y ppa:team-xbmc/ppa
+sudo add-apt-repository -y ppa:noobslab/themes #Flatabulous theme
+sudo add-apt-repository -y ppa:team-xbmc/ppa #kodi
+sudo add-apt-repository -y ppa:gerardpuig/ppa #ubuntu-cleaner
 sudo apt-add-repository -y "deb http://repository.spotify.com stable non-free" && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2C19886
 sudo apt-get -y update
-sudo apt-get -y install docky macbuntu-os-ithemes-lts-v7 gcc make valgrind gdb cgdb gedit git gedit-plugins libncurses5-dev libncurses5 cppcheck ncurses-doc vim nano mcedit synaptic arduino fritzing virtualbox atom indicator-netspeed unity-tweak-tool chromium-browser nautilus-dropbox alacarte shutter gimp vlc compizconfig-settings-manager deluge inkscape calibre htop xclip exfat-utils exfat-fuse tlp tlp-rdw gparted smartgit git-cola oracle-java8-installer intellij-idea-community codeblocks spotify-client fastboot adb ubuntu-restricted-extras software-properties-common kodi
+sudo apt-get -y install docky macbuntu-os-ithemes-lts-v7 flatabulous-theme gcc make valgrind gdb cgdb gedit git gedit-plugins libncurses5-dev libncurses5 cppcheck ncurses-doc vim nano mcedit synaptic arduino fritzing virtualbox atom indicator-netspeed unity-tweak-tool chromium-browser nautilus-dropbox alacarte shutter gimp vlc compizconfig-settings-manager deluge inkscape calibre htop xclip exfat-utils exfat-fuse tlp tlp-rdw gparted smartgit git-cola oracle-java8-installer intellij-idea-community codeblocks spotify-client fastboot adb ubuntu-restricted-extras software-properties-common kodi pepperflashplugin-nonfree freshplayerplugin ubuntu-cleaner rhythmbox-plugin-alternative-toolbar touchegg
 
 # Config system settings
 gsettings set com.ubuntu.sound allow-amplified-volume true
@@ -45,34 +90,59 @@ gsettings set com.canonical.Unity.FilesLens use-locate false
 gsettings set com.canonical.Unity.Lenses remote-content-search 'none'
 gsettings set com.canonical.Unity.Launcher favorites "['application://nautilus.desktop', 'application://gnome-terminal.desktop', 'application://synaptic.desktop', 'application://org.gnome.Software.desktop', 'application://virtualbox.desktop', 'application://libreoffice-writer.desktop', 'application://libreoffice-calc.desktop', 'application://libreoffice-impress.desktop', 'application://deluge.desktop', 'application://chromium-browser.desktop', 'application://chrome-hmjkmjkepdijhoojdojkdfohbdgmmhki-Default.desktop', 'application://firefox.desktop', 'unity://running-apps', 'application://cheese.desktop', 'unity://desktop-icon', 'unity://expo-icon', 'unity://devices']"
 gsettings set org.gnome.desktop.interface cursor-theme 'DMZ-Black'
-gsettings set org.gnome.desktop.interface gtk-theme 'MacBuntu-OS-Unity-Classic'
-gsettings set org.gnome.desktop.interface icon-theme 'My-Custom'
+gsettings set org.gnome.desktop.interface gtk-theme 'Flatabulous'
+gsettings set org.gnome.desktop.interface icon-theme 'My-Custom-Dark'
+gsettings set org.gnome.desktop.background picture-uri 'file:///home/rastik/Pictures/Wallpapers/Chamonix.jpg'
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
 gsettings set org.gnome.desktop.wm.preferences auto-raise true
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Alt>Shift_L']"
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'sk+qwerty')]"
+gsettings set org.gnome.rhythmbox.plugins active-plugins "['mmkeys', 'notification', 'rb', 'artsearch', 'generic-player', 'mtpdevice', 'android', 'alternative-toolbar', 'audioscrobbler', 'iradio', 'power-manager', 'audiocd', 'ipod', 'daap', 'mpris', 'dbus-media-server']"
 gsettings set org.gnome.gedit.preferences.editor bracket-machting true
 gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
 gsettings set org.gnome.gedit.preferences.editor highlight-current-line true
 gsettings set org.gnome.gedit.preferences.editor tabs-size 4
-gsettings set org.gnome.gedit.preferences.editor scheme "cobalt"
+gsettings set org.gnome.gedit.preferences.editor scheme "oblivion"
 gsettings set org.gnome.gedit.plugins active-plugins "['spell', 'quickopen', 'modelines', 'wordcompletion', 'bracketcompletion', 'git', 'codecomment', 'filebrowser', 'snippets', 'docinfo', 'externaltools', 'terminal', 'smartspaces', 'time']"
 gsettings set org.gnome.nautilus.preferences executable-text-activation 'ask'
 gsettings set org.gnome.nautilus.window-state sidebar-width 180
 gsettings set org.gnome.metacity show-maximized-titlebars true
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 1
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 4
-gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ panel-opacity 0.74169743061065674
+gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ panel-opacity 0.74904942512512207
 gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ icon-size 40
 gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ background-color '#555753ff'
 gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-minimize-window true
 gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-hide-mode 1
 dconf write /org/gnome/mutter/center-new-windows true
+dconf write /org/gnome/settings-daemon/peripherals/mouse/locate-pointer true
 dconf write /org/compiz/profiles/unity/plugins/unityshell/alt-tab-bias-viewport false
 dconf write /org/compiz/profiles/unity/plugins/grid/bottom-left-corner-action 1
 dconf write /org/compiz/profiles/unity/plugins/grid/bottom-right-corner-action 3
 dconf write /org/compiz/profiles/unity/plugins/grid/top-left-corner-action 7
 dconf write /org/compiz/profiles/unity/plugins/grid/top-right-corner-action 9
+
+# Global dark theme
+cat > ~/.config/gtk-3.0/settings.ini << EOF
+[Settings]
+gtk-application-prefer-dark-theme=1
+EOF
+
+# Touchegg and palm detection config
+cat > ~/.xprofile << EOF
+# Touchegg setup
+synclient TapButton2=0
+synclient ClickFinger2=0
+synclient TapButton3=0
+synclient ClickFinger3=0
+synclient HorizTwoFingerScroll=0
+synclient VertTwoFingerScroll=0
+touchegg &
+
+# Palm detection setup
+xinput set-prop 13 "Synaptics Palm Detection" 1
+xinput set-prop "13" "Synaptics Palm Dimensions" 5, 5
+EOF
 
 # Remove bloat
 sudo apt-get -y remove --purge aisleriot gnome-mahjongg gnome-mines gnome-sudoku gnome-calendar checkbox-gui totem example-content transmission-common transmission-gtk brltty onboard xdiagnose webbrowser-app apport

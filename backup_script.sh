2c3aeffe899ba855b4a246dbbffb43dc4b54f640
vangyyy <rasto.vangor@gmail.com>
2017-05-24 20:19:02 +0200

    Update Idea folder in backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # What to backup
-backup_files="/home/$USER/.ssh /home/$USER/.gimp-2.8 /home/$USER/.thunderbird /home/$USER/.mozilla /home/$USER/.atom /home/$USER/.bashrc /home/$USER/.gitconfig /home/$USER/.smartgit /home/$USER/.shutter /home/$USER/.gconf /home/$USER/.gnome /home/$USER/.gnome2 /home/$USER/.compiz /home/$USER/.IdeaIC2016.2 /home/$USER/.local /home/$USER/.config /home/$USER/Pictures /home/$USER/Calibre_library /home/$USER/bin /usr/share/icons/My-Custom /usr/share/applications/android-studio.desktop /usr/share/applications/intellij-idea-community.desktop"
+backup_files="/home/$USER/.ssh /home/$USER/.gimp-2.8 /home/$USER/.thunderbird /home/$USER/.mozilla /home/$USER/.atom /home/$USER/.bashrc /home/$USER/.gitconfig /home/$USER/.smartgit /home/$USER/.shutter /home/$USER/.gconf /home/$USER/.gnome /home/$USER/.gnome2 /home/$USER/.compiz /home/$USER/.IdeaIC2017.1 /home/$USER/.local /home/$USER/.config /home/$USER/Pictures /home/$USER/Calibre_library /home/$USER/bin /usr/share/icons/My-Custom /usr/share/applications/android-studio.desktop /usr/share/applications/intellij-idea-community.desktop"
 
 # Where to backup to
 dest="/home/$USER/Backups"

96ab90b1a204573ef0e1a0958b89425bcfaad204
vangyyy <rasto.vangor@gmail.com>
2018-06-27 13:22:53 +0200

    script can now be used multiple times & added banners

--- setup_18.04.sh
+++ setup_18.04.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-if [[ ($1 != "yoga") && ($1 != "desktop") ]]; then
-	echo "Error: missing/invalid operand"
-	echo "Usage: setup [yoga/desktop]"
-	exit
-fi
+echo -e '\n/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~ Adding repositories, update, upgrade ~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/\n'
 
-# Add repositories
 sudo add-apt-repository -y ppa:webupd8team/java
-
-# Update and Upgrade
 sudo apt -y update
 sudo apt -y upgrade
 
-# dropbox, docky, synergy
+echo -e '\n/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~ Installing apt packages ~~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~ TO DO: dropbox, docky, synergy ~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/\n'
 
 sudo apt install -y git gnome-session gnome-tweak-tool gnome-system-monitor gedit-plugins baobab rhythmbox thunderbird gimp virtualbox deluge vlc chromium-browser gparted adb oracle-java8-installer texlive-full unrar curl
 
-sudo snap remove gnome-system-monitor gnome-calculator gnome-characters gnome-logs
+echo -e '\n/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~ Installing snap packages ~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/\n'
+
 sudo snap install communitheme insomnia postman spotify
 sudo snap install --classic atom
 sudo snap install --classic phpstorm
@@ -25,14 +26,23 @@ sudo snap install --classic intellij-idea-ultimate
 sudo snap install --classic android-studio
 sudo snap install --classic slack
 
+echo -e '\n/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~~~~ Cloning Icon pack ~~~~~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/\n'
+
 git clone https://github.com/vangyyy/My-Custom-Gnome.git $HOME/.icons/My-Custom-Gnome
 
-# Config system settings
-gsettings set org.gnome.desktop.interface show-battery-percentage  true
-gsettings set org.gnome.desktop.interface enable-hot-corners false
-gsettings set org.gnome.desktop.interface clock-show-date  true
+echo -e '\n/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~~ Configuring gsettings ~~~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/\n'
+
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-format 24h
 gsettings set org.gnome.desktop.interface clock-show-seconds true
+gsettings set org.gnome.desktop.interface scaling-factor 1
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
+gsettings set org.gnome.desktop.interface icon-theme 'My-Custom-Gnome'
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 gsettings set org.gnome.desktop.peripherals.touchpad click-method 'fingers'
 gsettings set org.gnome.desktop.background show-desktop-icons true
@@ -43,7 +53,6 @@ gsettings set org.gnome.desktop.background picture-uri 'file:////usr/share/gnome
 gsettings set org.gnome.desktop.wm.preferences button-layout "close,minimize,maximize:"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Alt>Shift_L']"
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'sk+qwerty')]"
-gsettings set org.gnome.desktop.interface icon-theme 'My-Custom-Gnome'
 gsettings set org.gnome.settings-daemon.peripherals.mouse locate-pointer true
 gsettings set org.gnome.settings-daemon.peripherals.touchscreen orientation-lock true
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
@@ -64,28 +73,22 @@ gsettings set org.gnome.gedit.preferences.editor tabs-size 4
 gsettings set org.gnome.gedit.preferences.editor scheme "oblivion"
 gsettings set org.gnome.gedit.plugins active-plugins "['spell', 'quickopen', 'modelines', 'wordcompletion', 'bracketcompletion', 'git', 'codecomment', 'filebrowser', 'snippets', 'docinfo', 'externaltools', 'terminal', 'smartspaces', 'time']"
 
-# gnome-shell extensions install
+echo -e '\n/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~ Installing Gnome extensions ~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/\n'
+
 sudo wget -O /usr/local/bin/gnomeshell-extension-manage "https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/ubuntugnome/gnomeshell-extension-manage"
 sudo chmod +x /usr/local/bin/gnomeshell-extension-manage
-#gnomeshell-extension-manage --install --extension-id 442 --version 3.22 --user #Drop Down Terminal
-gnomeshell-extension-manage --install --extension-id 104 --version 3.26 --user #NetSpeed
+gnomeshell-extension-manage --install --extension-id 104 --version 3.28 --user #NetSpeed
 gnomeshell-extension-manage --install --extension-id 1128 --version 3.22 --user #Hide Activities Button
 gnomeshell-extension-manage --install --extension-id 112 --version 3.18 --user #Remove Accessibility
-gnomeshell-extension-manage --install --extension-id 723 --version 3.22 --user #Pixel Saver
-#gnomeshell-extension-manage --install --extension-id 805 --version 3.22 --user #Hide Dash X
+gnomeshell-extension-manage --install --extension-id 1267 --version 3.26 --user #No Title Bar
 gnomeshell-extension-manage --install --extension-id 808 --version 3.22 --user #Hide Workspace Thumbnails
 gnomeshell-extension-manage --install --extension-id 1011 --version 3.22 --user #Dynamic Panel Transparency
 
-# Pixel Saver workaround
-cd ~/.local/share/gnome-shell/extensions/pixel-saver@deadalnix.me/themes
-mv Ambiance Ambiance.bak
-ln -s default Ambiance
-
-# Global dark theme
-cat > ~/.config/gtk-3.0/settings.ini << EOF
-[Settings]
-gtk-application-prefer-dark-theme=1
-EOF
+echo -e '\n/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~~~~~~ GRUB settings ~~~~~~~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/\n'
 
 # Change grub background and default OS to Windows
 sudo bash -c 'cat << EOF > /usr/share/plymouth/themes/ubuntu-logo/ubuntu-logo.grub
@@ -96,33 +99,41 @@ EOF'
 sudo sed -i 's/GRUB_DEFAULT=0/GRUB_DEFAULT=2/' /etc/default/grub
 sudo update-grub
 
-# Lock-screen scaling
-sudo bash -c 'cat << EOF > /usr/share/glib-2.0/schemas/30_my-text-factor.gschema.override
-[org.gnome.desktop.interface]
-scaling-factor=1
-text-scaling-factor=1.25
-EOF'
-sudo glib-compile-schemas /usr/share/glib-2.0/schemas
+echo -e '\n/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~ Device specific settings ~~~~~~~~~~~~~~~~~~~~/'
 
-if [[ ($1 == "yoga") ]]; then
+if grep -Fxq 31 '/sys/class/dmi/id/chassis_type'; then
+	echo -e '/~~~~~~~~~~~~~~~~~~~~~~~ Thinkpad X1 Yoga ~~~~~~~~~~~~~~~~~~~~~~~~~/'
+	echo -e '/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/\n'
+
 	# Disable input sources
-	echo 'while read id; do xinput disable $id; done <<< $(xinput | grep 'Wacom' | cut -d"=" -f2 | cut -f1)' >> ~/.profile
+	line='while read id; do xinput disable $id; done <<< $(xinput | grep 'Wacom' | cut -d"=" -f2 | cut -f1)'
+	file=$HOME/.profile
+	grep -qF "$line" "$file" || echo -e "\n# Disable input sources\n$line" >> "$file"
 
 	# Trackpoint sensitivity
 	sudo bash -c 'cat << EOF > /etc/udev/rules.d/10-trackpoint.rules
-	ACTION=="add", SUBSYSTEM=="input", ATTR{name}=="TPPS/2 IBM TrackPoint", ATTR{device/speed}="255"
-	EOF'
+ACTION=="add", SUBSYSTEM=="input", ATTR{name}=="TPPS/2 IBM TrackPoint", ATTR{device/speed}="255"
+EOF'
+
 fi
 
-if [[ ($1 == "desktop") ]]; then
+if grep -Fxq 3 '/sys/class/dmi/id/chassis_type'; then
+	echo -e '/~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Desktop ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/'
+	echo -e '/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/\n'
+
 	# Install Nvidia drivers
 	sudo ubuntu-drivers autoinstall
+
 	# Disable suspend
 	gsettings set org.gnome.desktop.session idle-delay 'uint32 0'
 fi
 
-# Clean
+echo -e '\n/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~~~~~ Removing packages ~~~~~~~~~~~~~~~~~~~~~~~/'
+echo -e '/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/\n'
+
+sudo snap remove gnome-system-monitor gnome-calculator gnome-characters gnome-logs
 sudo apt purge apport memtest86+
 sudo apt -y autoremove
 sudo apt -y clean
-
--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# sudo sed -i 'N;s/<allow\ssend_destination="org.freedesktop.UPower"\n\s\s\s\s\s\s\s\s\s\s\ssend_interface="org.freedesktop.UPower.KbdBacklight"/<deny send_destination="org.freedesktop.UPower"\n           send_interface="org.freedesktop.UPower.KbdBacklight"/' /etc/dbus-1/system.d/org.freedesktop.UPower.conf
-
 # duplicity file:///home/rastik/Desktop/Backups/deja-dup/ ~/Desktop/abc
 
 #sudo bash -c 'cat << EOF > /etc/tmpfiles.d/tpoint.conf
@@ -9,3 +7,27 @@
 #w /sys/devices/platform/i8042/serio1/serio2/sensitivity - - - - 255
 #EOF'
 #sudo systemd-tmpfiles --prefix=/sys --create
+
+# Global dark theme
+#cat > $HOME/.config/gtk-3.0/settings.ini << EOF
+#[Settings]
+#gtk-application-prefer-dark-theme=1
+#EOF
+
+
+
+#echo -e '\n/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/'
+#echo -e '/~~~~~~~~~~~~~~~~~~~~~~ Applying glib schemas ~~~~~~~~~~~~~~~~~~~~~/'
+#echo -e '/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/\n'
+
+# Lockscreen and display scaling
+#sudo bash -c 'cat << EOF > /usr/share/glib-2.0/schemas/30_my-text-factor.gschema.override
+#[org.gnome.desktop.interface]
+#scaling-factor=1
+#text-scaling-factor=1.25
+#EOF'
+#sudo glib-compile-schemas /usr/share/glib-2.0/schemas
+
+
+
+#sudo bash -c 'echo -e "ACTION==\"add\", SUBSYSTEM==\"input\", ATTR{name}==\"TPPS/2 IBM TrackPoint\", ATTR{device/speed}=\"255\"" >> /etc/udev/rules.d/10-trackpoint.rules'
